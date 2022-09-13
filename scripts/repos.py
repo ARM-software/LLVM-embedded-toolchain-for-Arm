7b99cbc81e42bd41146f557afd9a73c8ea857aa0
Michael Platings <michael.platings@arm.com>
2022-09-13 15:21:52 +0100

    Fix re-patching picolibc
    Applying the picolibc patch creates new files.
    If those files exist when applying the patch a second time then patching
    will fail because Git refuses to overwrite existing files.

--- scripts/repos.py
+++ scripts/repos.py
@@ -239,8 +239,8 @@ def patch_repositories(checkout_path: str, tc_version: LLVMBMTC,
         repo = git.Repo(repo_path)
         try:
             repo.head.reset(index=True, working_tree=True)
-            # Remove ignored files
-            repo.git.clean(['-fX'])
+            # Remove untracked files and directories
+            repo.git.clean(['-fdx'])
             repo.git.apply(['-p1', patch_file])
         except git.exc.GitCommandError as ex:  # pylint: disable=no-member
             die('could not patch "{}" with "{}".\n'
