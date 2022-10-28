67085ff927b37e5815f0dadc1cb57c90def65dbf
Michael Platings <michael.platings@arm.com>
2022-10-28 12:51:40 +0100

    Enable localisation
    Doing so requires enabling GNU extensions in picolibc since libc++ uses
    functions like strtof_l that take a locale argument. These functions are
    called from inline functions so we must enable them globally instead of
    passing an argument only when building libc++. An alternative option is
    to set _GNU_SOURCE in libc++'s __config_site header but that would mean
    different sets of functions would be declared depending on the ordering
    of includes.
    
    Picolibc's tinystdio doesn't include strtoll_l or strtoull_l so this
    change adds those. They behave in the same way as tinystdio's strtof_l
    which discards the locale argument.
    
    The C++ smoke test now includes all the C++ headers it can to make it
    more obvious what can and cannot be included. As you'd expect,
    filesystem, threading and wchar related headers can't be included since
    we've explicitly disabled those features.

--- tests/smoketests/cpp-basic-semihosting/hello.cpp
+++ tests/smoketests/cpp-basic-semihosting/hello.cpp
@@ -1,9 +1,96 @@
+// Include as many C++17 headers as possible.
+// Unsupported headers are commented out.
+#include <algorithm>
+#include <any>
+#include <array>
+#include <atomic>
+#include <bitset>
+#include <chrono>
+#include <codecvt>
+#include <complex>
+#include <condition_variable>
+#include <deque>
+#include <exception>
+#include <execution>
+//#include <filesystem>
+#include <forward_list>
+#include <fstream>
+#include <functional>
+//#include <future>
+#include <initializer_list>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <memory_resource>
+//#include <mutex>
+#include <new>
+#include <numeric>
+#include <optional>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <set>
+//#include <shared_mutex>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
 #include <string>
+#include <string_view>
+#include <strstream>
+#include <system_error>
+//#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <variant>
+#include <vector>
+
+#include <cassert>
+#include <cinttypes>
+#include <csignal>
 #include <cstdio>
+//#include <cwchar>
+#include <ccomplex>
+#include <ciso646>
+//#include <cstdalign> // removed in C++20
+#include <cstdlib>
+//#include <cwctype>
+#include <cctype>
+#include <climits>
+#include <cstdarg>
+#include <cstring>
+#include <cerrno>
+#include <clocale>
+#include <cstdbool>
+#include <ctgmath>
+#include <cfenv>
+#include <cmath>
+#include <cstddef>
+#include <ctime>
+#include <cfloat>
+#include <csetjmp>
+#include <cstdint>
+#include <cuchar>
 
 int main(void) {
-    std::string str = "Hello World!\n";
-    printf("%s", str.c_str());
+    std::string str = "Hello World!";
+    std::cout << str << std::endl;
     return 0;
 }
-
