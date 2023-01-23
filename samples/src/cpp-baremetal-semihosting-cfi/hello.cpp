1ea991d22928ceafe097102c06c7ae297ea3d7a7
Volodymyr Turanskyy <66017160+voltur01@users.noreply.github.com>
2023-01-23 09:31:31 +0000

    Add CFI sample (#175)
    * Add CFI sample
    
    * Fix copyright year
    
    * Add to the list of samples in cmake

---
+++ samples/src/cpp-baremetal-semihosting-cfi/hello.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Base {
+  public:
+    virtual ~Base() {}
+    virtual void print_type() {
+      std::cout << "Base" << std::endl;
+    }
+};
+
+class Good: public Base {
+  public:
+    void print_type() override {
+      std::cout << "Good" << std::endl;
+    }
+};
+
+class Bad { // not derived from Base
+  public:
+    virtual ~Bad() {}
+    virtual void print_type() {
+      std::cout << "Bad" << std::endl;
+    }
+};
+
+int main(void) {
+  Base* base_ptr = reinterpret_cast<Good*>(new Bad());
+  base_ptr->print_type();
+  
+  std::cout << "C++ CFI sample" << std::endl;
+
+  return 0;
+}
