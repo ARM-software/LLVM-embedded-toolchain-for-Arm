b36bd84175c9020f31d0364e53ca8f452297f25f
Michael Platings <michael.platings@arm.com>
2023-06-12 16:27:41 +0100

    Multilib
    This change adds multilib configuration in accordance with the
    documentation in https://reviews.llvm.org/D143587
    
    For the purposes of testing the multilib configuration, this change adds
    lit test configuration. It is intended that this test configuration will
    make it easier to add tests for other purposes as well, instead of
    adding targets in directly in CMake.

---
+++ tests/lit.cfg.py
@@ -0,0 +1,20 @@
+# -*- Python -*-
+
+import os
+
+import lit.formats
+
+from lit.llvm import llvm_config
+
+# Configuration file for the 'lit' test runner.
+
+config.name = 'LLVM Embedded Toolchain for Arm'
+config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
+config.suffixes = ['.test']
+config.excludes = ['CMakeLists.txt', 'Makefile.conf', 'ldscripts', 'smoketests']
+config.test_source_root = os.path.dirname(__file__)
+
+llvm_config.use_default_substitutions()
+llvm_config.use_clang()
+
+config.environment["CLANG_NO_DEFAULT_CONFIG"] = "1"
