10853e8b8b8284f88f0ed501c74966e29df9f913
Dominik Wójt <dominik.wojt@arm.com>
2023-06-22 17:25:11 +0200

    Run test with QEMU same way as picolibc tests do
    The lit-exec-qemu.py script was modified to reproduce the QEMU calls
    used in picolibc's own tests.

--- test-support/lit-exec-qemu.py
+++ test-support/lit-exec-qemu.py
@@ -7,58 +7,107 @@
 # arguments as llvm-project/libcxx/utils/run.py.
 
 import sys
-import re
 import argparse
 import subprocess
 import pathlib
 
-# This script requires Python 3.6 or later
-assert sys.version_info >= (3, 6)
-
 
 def run(args):
     """Execute the program using QEMU and return the subprocess return code."""
-    command = args.command
-    assert command
-    if command[0] == '--':
-        command = command[1:]
-    image = command[0]
-    image_args = command[1:]
-    qemu_params = args.qemu_params.split(':')
-    qemu_cmd = ([args.qemu_command] + qemu_params +
-        ['-semihosting', '-nographic', '-kernel', image])
-    if image_args:
-        image_args_concat = ','.join(['arg=' + arg.replace(',', ',,')
-                                      for arg in image_args])
-        qemu_cmd += ['-semihosting-config', image_args_concat]
-    result = subprocess.run(qemu_cmd, stdout=subprocess.PIPE,
-                            stderr=sys.stderr,
-                            timeout=args.timeout,
-                            cwd=args.execdir,
-                            check=False)
+    qemu_params = ["-M", args.qemu_machine]
+    if args.qemu_cpu:
+        qemu_params += ["-cpu", args.qemu_cpu]
+    if args.qemu_params:
+        qemu_params += args.qemu_params.split(":")
+
+    # Setup semihosting with chardev bound to stdio.
+    # This is needed to test semihosting functionality in picolibc.
+    qemu_params += ["-chardev", "stdio,mux=on,id=stdio0"]
+    semihosting_config = ["enable=on", "chardev=stdio0"] + [
+        "arg=" + arg.replace(",", ",,") for arg in args.arguments
+    ]
+    qemu_params += ["-semihosting-config", ",".join(semihosting_config)]
+
+    # Disable features we don't need and which could slow down the test or
+    # interfere with semihosting.
+    qemu_params += ["-monitor", "none", "-serial", "none", "-nographic"]
+
+    # Load the image to machine's memory and set the PC.
+    # "virt" machine cannot be used with load, as QEMU will try to put
+    # device tree blob at start of RAM conflicting with our code
+    # https://www.qemu.org/docs/master/system/arm/virt.html#hardware-configuration-information-for-bare-metal-programming
+    if args.qemu_machine == "virt":
+        qemu_params += ["-kernel", args.image]
+    else:
+        qemu_params += ["-device", f"loader,file={args.image},cpu-num=0"]
+
+    qemu_cmd = [args.qemu_command] + qemu_params
+    result = subprocess.run(
+        qemu_cmd,
+        stdout=subprocess.PIPE,
+        stderr=sys.stderr,
+        timeout=args.timeout,
+        cwd=args.execdir,
+        check=False,
+    )
     sys.stdout.buffer.write(result.stdout)
     return result.returncode
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Run a single test using qemu')
-    parser.add_argument('--qemu-command', required=True, help='qemu-system-<arch> path')
-    parser.add_argument('--qemu-params', required=True,
-                        help='list of arguments to pass to qemu, separated with ":"')
-    parser.add_argument('--timeout', type=int, default=60,
-                        help='timeout, in seconds (default: 60)')
-    parser.add_argument('--execdir', type=pathlib.Path, default='.',
-                        help='directory to run the program from')
-    parser.add_argument('--codesign_identity', type=str,
-                        help='ignored, used for compatibility with libc++ tests')
-    parser.add_argument('--env', type=str, nargs='*',
-                        help='ignored, used for compatibility with libc++ tests')
-    parser.add_argument('command', nargs=argparse.REMAINDER,
-                        help='image file to execute with optional arguments')
+    parser = argparse.ArgumentParser(
+        description="Run a single test using qemu"
+    )
+    parser.add_argument(
+        "--qemu-command", required=True, help="qemu-system-<arch> path"
+    )
+    parser.add_argument(
+        "--qemu-machine",
+        required=True,
+        help="name of the machine to pass to QEMU",
+    )
+    parser.add_argument(
+        "--qemu-cpu", required=False, help="name of the cpu to pass to QEMU"
+    )
+    parser.add_argument(
+        "--qemu-params",
+        required=False,
+        help='list of arguments to pass to qemu, separated with ":"',
+    )
+    parser.add_argument(
+        "--timeout",
+        type=int,
+        default=60,
+        help="timeout, in seconds (default: 60)",
+    )
+    parser.add_argument(
+        "--execdir",
+        type=pathlib.Path,
+        default=".",
+        help="directory to run the program from",
+    )
+    parser.add_argument(
+        "--codesign_identity",
+        type=str,
+        help="ignored, used for compatibility with libc++ tests",
+    )
+    parser.add_argument(
+        "--env",
+        type=str,
+        nargs="*",
+        help="ignored, used for compatibility with libc++ tests",
+    )
+    parser.add_argument("image", help="image file to execute")
+    parser.add_argument(
+        "arguments",
+        nargs=argparse.REMAINDER,
+        default=[],
+        help="optional arguments for the image",
+    )
     args = parser.parse_args()
     ret_code = run(args)
     sys.exit(ret_code)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
