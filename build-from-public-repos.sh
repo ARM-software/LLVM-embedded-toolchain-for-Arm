1bb0ae061cc266d1abd97c24a6e797d7f00f62a1
Pawel Wodnicki <pawel.wodnicki@live.com>
2024-06-14 12:49:41 -0500

    Fix build script

--- build-from-public-repos.sh
+++ build-from-public-repos.sh
@@ -2,9 +2,9 @@
 
 mkdir repos
 git -C repos clone https://github.com/32bitmicro/llvm-project.git
-git -C repos/llvm-project am -k "$PWD"/../patches/llvm-project/*.patch
+git -C repos/llvm-project am -k "$PWD"/patches/llvm-project/*.patch
 git -C repos clone https://github.com/32bitmicro/picolibc.git
-git -C repos/picolibc apply "$PWD"/../patches/picolibc.patch
+git -C repos/picolibc apply "$PWD"/patches/picolibc.patch
 mkdir build-from-repos
 cd build-from-repos
 cmake .. -GNinja -DFETCHCONTENT_SOURCE_DIR_LLVMPROJECT=../repos/llvm-project -DFETCHCONTENT_SOURCE_DIR_PICOLIBC=../repos/picolibc
